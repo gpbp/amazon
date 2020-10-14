26f9e4e1c5154dcacbed62d386cc1bf783b89bb7
Pham Hai Trung <haitrung.pham6573@gmail.com>
2020-10-14 18:13:07 +0200

    add redux to cart page

--- amazon-front/src/Screens/ProductScreen.js
+++ amazon-front/src/Screens/ProductScreen.js
@@ -66,7 +66,7 @@ function ProductScreen(props) {
                             </select>
                         </li>
                                 <li>
-                                    {product.countInStock > 0 && <button onClick={handleAddToCart} className="button">Add to cart</button>}
+                                    {product.countInStock > 0 && <button onClick={handleAddToCart} className="button primary">Add to cart</button>}
                         </li>
                     </ul>
                 </div>
