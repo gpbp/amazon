12cfbb71f21ac1543f541bc1183187f5c74503b2
Pham Hai Trung <haitrung.pham6573@gmail.com>
2020-10-14 08:44:42 +0200

    add redux to product detail page

--- amazon-back/server.js
+++ amazon-back/server.js
@@ -3,18 +3,20 @@ import data from './data';
 
 const app = express();
 
-app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
-
 app.get("/api/products", (req, res) => {
     res.send(data.products);
 });
 
-app.get("/", (req, res) => {
-    res.send("It's work");
-});
-
-app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+app.get("/api/products/:id", (req, res) => {
+    const productId = req.params.id;
+    const product = data.products.find(x => x._id === productId);
+    if (product) {
+        res.send(data.products.find(x => x._id === productId));
+    }
+    else {
+        res.status
+    }
+    
 });
 
 app.listen(5000, () => {console.log("Server started at http://localhost:5000")})
--- amazon-front/src/actions/productActions.js
+++ amazon-front/src/actions/productActions.js
@@ -1,4 +1,11 @@
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from "../constants/productConstants";
+import {
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_REQUEST
+} from "../constants/productConstants";
 import axios from 'axios';
 
 const listProducts = () => async (dispatch) => {
@@ -11,4 +18,16 @@ const listProducts = () => async (dispatch) => {
     dispatch({type: PRODUCT_LIST_FAIL, payload: error.message});
   }
 }
-export {listProducts};
+
+const detailsProduct = (productId) => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
+        const { data } = await axios.get("/api/products/" + productId);
+        dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+    }
+    catch (error) {
+        dispatch({type: PRODUCT_DETAILS_FAIL, payload: error.message})
+    }
+}
+
+export {listProducts, detailsProduct};
